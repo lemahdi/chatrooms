750650ba8f9e47928a291d404ae5a0e09aa02008
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-12-08 13:34:01 +0100

    processing raw user input

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -5,3 +5,21 @@ function divEscapedContentElement(message) {
 function divSystemContentElement(message) {
     return ^('<div></div>').html('<i>' + message + '</i>');
 }
+
+function processUserInput(chatApp, socket) {
+    var message = $('#send-message').val();
+    var systemMEssage;
+
+    if (message.charAt(0) == '/') {
+	systemMessage = chatApp.processCommand(message);
+	if (systemMessage) {
+	    $('#messages').append(divSystemContentElement(systemMessage));
+	}
+    } else {
+	chatApp.sendMessage($('#room').text(), message);
+	$('#messages').append(divEscapedContentElement(message));
+	$('#messages').scrollTop($('#messages').prop('scrollHeight'));
+    }
+
+    $('#send-message').val('');
+}
