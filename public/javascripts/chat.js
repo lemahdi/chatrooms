7aed4d698e64dfac3ce96fd4a4df73721bb62ebe
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-12-08 17:43:23 +0100

    indentation

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,46 +1,46 @@
 var Chat = function(socket) {
-    this.socket = socket;
+  this.socket = socket;
 };
 
 Chat.prototype.sendMessage = function(room, text) {
-    var message = {
+  var message = {
 	room: room,
 	text: text
-    };
-    this.socket.emit('message', message);
+  };
+  this.socket.emit('message', message);
 };
 
 
 Chat.prototype.changeRoom = function(room) {
-    this.socket.emit('join', {
-	    newRoom: room
-	});
+  this.socket.emit('join', {
+    newRoom: room
+  });
 };
 
 Chat.prototype.processCommand = function(command) {
-    var words = command.split(' ');
-var command = words[0]
-    .substring(1, words[0].length)
-    .toLowerCase();
-    var message = false;
+  var words = command.split(' ');
+  var command = words[0]
+                    .substring(1, words[0].length)
+                    .toLowerCase();
+  var message = false;
 
-    switch(command) {
-    case 'join':
+  switch(command) {
+  case 'join':
 	words.shift();
 	var room = words.join(' ');
 	this.changeRoom(room);
 	break;
 
-    case 'nick':
+  case 'nick':
 	words.shift();
 	var name = words.join(' ');
 	this.socket.emit('nameAttempt', name);
 	break;
 
-    default:
+  default:
 	message = 'Unrecognized command.';
 	break;
-    }
+  }
 
-    return message;
+  return message;
 };
